9608d90212b2544ad903724294cd26acf0253604
DaKingLawson <DaKingLawson@users.noreply.github.com>
2022-11-15 23:22:04 -0600

    Updated Waitlist Styling

--- index.js
+++
@@ -1,9 +0,0 @@
-
-document.querySelectorAll('a.nav').forEach(el => {
-  el.onclick = event => {
-    document.querySelectorAll('main').forEach(main => {
-      main.classList.toggle('hidden', main.id !== event.target.href.split('#')[1]);
-    });
-    return false;
-  }
-});
