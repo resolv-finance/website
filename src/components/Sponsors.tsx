b13e7295c580691f3ac9b9186fbd996456626f10
Neel <neelk2@illinois.edu>
2024-08-26 11:59:12 -0500

    fix mobile styles

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -8,18 +8,30 @@ import Image from "next/image";
 const Sponsors = () => {
   return (
     <div className="relative w-screen left-1/2 right-1/2 -mx-[50vw]">
-      <div className="max-w-screen-2xl mx-auto px-16">
-        <div className="flex w-full bg-stoneGray rounded-[5rem] ring ring-stoneGrayDark ring-1 p-8">
-          <div className="flex w-full gap-8 h-8 items-center justify-around">
+      <div className="max-w-screen-2xl mx-auto px-4">
+        <div className="flex w-full bg-stoneGray rounded-[5rem] ring ring-stoneGrayDark ring-1 p-8 overflow-x-auto">
+          <div className="flex flex-nowrap w-full justify-between gap-8 items-center min-w-max">
             <div className="shrink-0">
               Backed by world-class
               <br />
               investors, including
             </div>
-            <Image src={uiucicon} alt="UIUC" />
-            <Image src={orangedaoicon} alt="Orange DAO" />
-            <Image src={uchicagoicon} alt="University of Chicago" />
-            <Image src={ocaventuresicon} alt="OCA Ventures" />
+            <Image src={uiucicon} alt="UIUC" className="sm:h-auto w-auto h-6" />
+            <Image
+              src={orangedaoicon}
+              alt="Orange DAO"
+              className="sm:h-auto w-auto h-6"
+            />
+            <Image
+              src={uchicagoicon}
+              alt="University of Chicago"
+              className="sm:h-auto w-auto h-6"
+            />
+            <Image
+              src={ocaventuresicon}
+              alt="OCA Ventures"
+              className="sm:h-auto w-auto h-6"
+            />
           </div>
         </div>
       </div>
