f51c236f7417305520be1e17849d2b5122cb5acf
APex <37938428+APexBlockchainDevelopments@users.noreply.github.com>
2024-10-13 16:56:04 -0600

    new static site

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 module.exports = {
   // Enable static HTML export
   output: 'export',
-  
-  // Add assetPrefix to ensure that static assets are served from the S3 bucket
   assetPrefix: '/',
 
-  // If needed, you can configure a basePath for routing
-  // basePath: '/your-base-path',
-  
-  // Set up any other Next.js configurations here
 };
